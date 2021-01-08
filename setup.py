dbbc86e44672919963d921c8ab24db9984eb4eb4
Mikhas <meequz@gmail.com>
2021-01-08 18:31:47 +0300

    Exclude test projects from git

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 
 setup(
     name='meetg',
-    version='0.2',
+    version='0.2.1',
     packages=['meetg'],
     scripts=['bin/meetg-admin'],
     description='Framework for creating Telegram bots',
